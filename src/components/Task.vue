1a8af7d99d19152f1f0e8efdba9dc59ef6eb03b0
fujisawatk <fuji19880707@gmail.com>
2019-10-22 23:46:55 +0900

    design task view

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,8 +1,38 @@
 <template>
-  <div id="task">
-    タスクページ
+  <div class="todo-list card">
+    <div class="header light-blue darken-2 white-text">
+      <span>NotToDo List</span>
+    </div>
+    <div class="item">
+      <input type="checkbox" class="filled-in" checked="checked" />
+      <span id="text">Youtubeは一日1時間にする！！</span>
+      <div class="right">
+        <i class="material-icons small">edit</i>
+        <i class="material-icons small">delete</i>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
+.todo-list{
+  width: 70%;
+  margin: 1em auto;
+}
+.header {
+  padding: 1em;
+  font-size: 1.5em;
+}
+.item {
+  padding: 1em;
+  cursor: pointer;
+  font-size: 1.5em;
+}
+.item:hover {
+  background-color: #f3f3f3;
+}
+#text{
+  font-size: 1.25em;
+  color: black;
+}
 </style>
