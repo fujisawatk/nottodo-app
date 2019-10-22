3e9dd0d81ecb21e7e9fed1c4d41c2c14a0d1f29c
fujisawatk <fuji19880707@gmail.com>
2019-10-22 15:38:45 +0900

    add root auth

--- src/router.js
+++ src/router.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 import Task from '@/components/Task.vue'
 import Signup from '@/components/Signup'
 import Signin from '@/components/Signin'
+import firebase from "firebase/app";
+import "firebase/auth";
+
 
 Vue.use(Router)
 
@@ -10,22 +13,50 @@ let router = new Router({
   mode: 'history',
   routes: [
     {
+      path: '*',
+      redirect: 'signin'
+    },
+    {
       path: '/',
       name: 'task',
       component: Task,
+      // ルート認証がいる画面であることを宣言
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/signup',
       name: 'Signup',
-      component: Signup,
+      component: Signup
     },
     {
       path: '/signin',
       name: 'Signin',
-      component: Signin,
+      component: Signin
     }
   ]
 })
 
+// ルートガード
+router.beforeEach((to, from, next)=>{
+  // ルート認証があるかチェック
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  if (requiresAuth){
+    // ユーザー認証されているかを確認するメソッド
+    firebase.auth().onAuthStateChanged(function(user){
+      if(user){
+        next()
+      }else{
+        next({
+          path: '/signin',
+          query: {redirect: to.fullPath}
+        })
+      }
+    })
+  }else{
+    next()
+  }
+})
 
 export default router
