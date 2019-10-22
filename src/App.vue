765bb45626b02937717d6490f277fb0bdac2b57b
fujisawatk <fuji19880707@gmail.com>
2019-10-22 22:45:40 +0900

    add Navbar view

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <h1>NotToDoアプリ</h1>
+    <Navbar />
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
+</script>
+
+<style>
+
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="navbar">
+    <div class="nav-wrapper indigo darken-2">
+      <router-link to="">
+        <span class="brand-logo">NotToDo-app</span>
+      </router-link>
+
+      <ul class="right hide-on-med-and-down">
+        <li><router-link to="/signin">ログイン</router-link></li>
+        <li><router-link to="/signup">サインアップ</router-link></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
