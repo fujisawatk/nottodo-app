fea3089717dcba2fd13d6be484fdf7c86480b9f4
fujisawatk <fuji19880707@gmail.com>
2019-10-23 22:58:43 +0900

    setting firestore

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,8 +1,9 @@
 import firebase from 'firebase/app'
 import "firebase/auth"
+import "firebase/firestore"
 
 // Firebase設定データ
-var firebaseConfig = {
+const config = {
   apiKey: "AIzaSyDIaRYxHRM13TadGqValIVoH0iPgKGG7W4",
   authDomain: "nottodo-app.firebaseapp.com",
   databaseURL: "https://nottodo-app.firebaseio.com",
@@ -10,6 +11,11 @@ var firebaseConfig = {
   storageBucket: "nottodo-app.appspot.com",
   messagingSenderId: "104050327149",
   appId: "1:104050327149:web:470ccf48eaac45611e4964",
+  measurementId: "G-Z7P5XBVFFF"
 };
 // Firebase初期化
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(config);
+// Firestore設定
+const db = firebaseApp.firestore()
+// Timestampクラスに変換させるオプション
+db.settings({ timestampsInSnapshots: true })
